913de3ea0d793b6892e1500d230d28984aed8d9b
Ryan Kraft <rkraft3@students.towson.edu>
2023-10-16 12:43:14 -0400

    second commit. added title and header text

--- app/src/main/java/edu/towson/cosc435/kraft/sidequest/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc435/kraft/sidequest/MainActivity.kt
@@ -3,13 +3,20 @@ package edu.towson.cosc435.kraft.sidequest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.towson.cosc435.kraft.sidequest.ui.theme.SideQuestTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    mainScreen()
                 }
             }
         }
@@ -30,17 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun mainScreen(){
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Side Quest",
+            fontSize = 35.sp,
+            modifier = Modifier.padding(30.dp)
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(
+                text = "Current Quests",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+        Column(){
+            LazyColumn {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SideQuestTheme {
-        Greeting("Android")
+            }
+        }
     }
 }
---
+++ app/src/main/java/edu/towson/cosc435/kraft/sidequest/ui/theme/QuestRow.kt
@@ -0,0 +1,11 @@
+package edu.towson.cosc435.kraft.sidequest.ui.theme
+
+//import androidx.compose.runtime.Composable
+//
+//@Composable
+//fun QuestRow(
+//    id: Int,
+//    Quest: Quest,
+//    onComplete: (Quest) -> Unit,
+//    onFail: (Quest) -> Unit
+//)
