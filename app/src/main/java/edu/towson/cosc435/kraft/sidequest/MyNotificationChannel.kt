c00b357535464223b61360ff56737bf3d6104141
Ryan Kraft <rkraft3@students.towson.edu>
2023-12-10 18:24:52 -0500

    added comments to MyNotificationChannel and QuestCounterNotificationService

--- app/src/main/java/edu/towson/cosc435/kraft/sidequest/MyNotificationChannel.kt
+++ app/src/main/java/edu/towson/cosc435/kraft/sidequest/MyNotificationChannel.kt
@@ -6,24 +6,27 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 
+// my notification channel is used to set up the notification channel for the quest counter notification
 class MyNotificationChannel: Application() {
 
+    // overrides the on create function to set up the notification right when the application is launched
     override fun onCreate() {
-        super.onCreate()
-        createNotificationChannel()
+        super.onCreate() // runs all the normal code for the onCreate function
+        createNotificationChannel() // adds our function to on create to be executed after standard startup instructions are executed
     }
 
+    // function used to create the notification channel
     private fun createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){ // checks if the proper build version is installed to create notification channel
             val channel = NotificationChannel(
-                QuestCounterNotificationService.QUEST_COUNTER_CHANNEL_ID,
-                "quest counter",
-                NotificationManager.IMPORTANCE_DEFAULT
+                QuestCounterNotificationService.QUEST_COUNTER_CHANNEL_ID, // adds our quest counter notification service tothe channel
+                "quest counter", // names the channel quest counter
+                NotificationManager.IMPORTANCE_DEFAULT // sets the level of importance of the channel to the default level
             )
 
-            channel.description = "Used to count pending quests"
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+            channel.description = "Used to count pending quests" // sets the description of the channel to
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager // sets the notification manager using the context from the application
+            notificationManager.createNotificationChannel(channel) // creates the notification channel
         }
     }
 }
--- app/src/main/java/edu/towson/cosc435/kraft/sidequest/QuestCounterNotificationService.kt
+++ app/src/main/java/edu/towson/cosc435/kraft/sidequest/QuestCounterNotificationService.kt
@@ -7,44 +7,50 @@ import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 
+// class to build the notification that is sent from app
 class QuestCounterNotificationService(private val context: Context) {
 
-    private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    val activityIntent = Intent(context, MainActivity::class.java)
-    val pendingIntent = PendingIntent.getActivity(
-        context,
+    private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager // creates notification manager
+    private val activityIntent = Intent(context, MainActivity::class.java) // creates intent for pending intent
+    // creates the pending intent for notification
+    private val pendingIntent: PendingIntent = PendingIntent.getActivity(
+        context, // passes in context from application
         1,
-        activityIntent,
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        activityIntent, // passes in activity intent
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) // checks if the proper build version is installed and sets pending intent flag to immutable
             PendingIntent.FLAG_IMMUTABLE
         else
             0
     )
+
+    // companion object used to id the notification channel
     companion object{
         const val QUEST_COUNTER_CHANNEL_ID = "quest_counter_channel"
     }
 
+    // constructs notification and displays it to the user
+    // takes in the quest counter (number of pending quests) from the quest list view model
     fun showNotification(questCounter: Int){
-        if(questCounter > 0){
-            val notification = NotificationCompat.Builder(context, QUEST_COUNTER_CHANNEL_ID)
-                .setSmallIcon(R.drawable.baseline_checklist_24)
-                .setContentTitle("Quests Pending Count")
-                .setContentText("The total number of quests pending is $questCounter")
-                .setContentIntent(pendingIntent)
-                .setSilent(true)
-                .build()
+        if(questCounter > 0){ // checks if the count is greater than 0
+            val notification = NotificationCompat.Builder(context, QUEST_COUNTER_CHANNEL_ID) // instantiates notification object to be built
+                .setSmallIcon(R.drawable.baseline_checklist_24) // sets the icon of the notification
+                .setContentTitle("Quests Pending Count") // titles the notification
+                .setContentText("The total number of quests pending is $questCounter") // sets the text for the notification
+                .setContentIntent(pendingIntent) // sets the intent for the notification
+                .setSilent(true) // makes notification silent
+                .build() // builds the notification
 
-            notificationManager.notify(1, notification)
+            notificationManager.notify(1, notification) // sends notification
         } else {
-            val notification = NotificationCompat.Builder(context, QUEST_COUNTER_CHANNEL_ID)
-                .setSmallIcon(R.drawable.baseline_checklist_24)
-                .setContentTitle("Quests Pending Count")
-                .setContentText("You have no quest pending at the moment. Add a quest to hack a habit.")
-                .setContentIntent(pendingIntent)
-                .setSilent(true)
-                .build()
+            val notification = NotificationCompat.Builder(context, QUEST_COUNTER_CHANNEL_ID) // instantiates notification object to be built
+                .setSmallIcon(R.drawable.baseline_checklist_24) // sets the icon of the notification
+                .setContentTitle("Quests Pending Count") // titles the notification
+                .setContentText("You have no quest pending at the moment. Add a quest to hack a habit.") // sets the text for the notification
+                .setContentIntent(pendingIntent) // sets the intent for the notification
+                .setSilent(true) // makes notification silent
+                .build() // builds the notification
 
-            notificationManager.notify(1, notification)
+            notificationManager.notify(1, notification) // sends notification
         }
 
     }
